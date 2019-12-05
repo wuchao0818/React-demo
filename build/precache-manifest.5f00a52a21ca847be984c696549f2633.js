f2f442fbd217945681f677272c159e6448e3d8d2
吴超 <wuchao@wuchaodeMacBook-Pro.local>
2019-12-05 17:22:00 +0800

    打包

---
+++ build/precache-manifest.5f00a52a21ca847be984c696549f2633.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db26b19c6f80037c8f8ea58d3f6f4709",
+    "url": "/index.html"
+  },
+  {
+    "revision": "928a5c06988059dad3e6",
+    "url": "/static/css/2.7f669fb7.chunk.css"
+  },
+  {
+    "revision": "2482f4581253a676b93f",
+    "url": "/static/css/main.87d3e0b0.chunk.css"
+  },
+  {
+    "revision": "928a5c06988059dad3e6",
+    "url": "/static/js/2.19366f26.chunk.js"
+  },
+  {
+    "revision": "2482f4581253a676b93f",
+    "url": "/static/js/main.3abc8c41.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "aacf048a75278d20e5e6cc39dee43942",
+    "url": "/static/media/photo.aacf048a.jpeg"
+  }
+]);
